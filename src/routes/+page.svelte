8a44f93a86e283db8d9ee79898a484bcfc86dffd
Taylor Dawson <taylorjdawson@gmail.com>
2023-05-01 15:09:07 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Scatter } from 'svelte-chartjs';
+	import { data } from '../data';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		LineElement,
+		CategoryScale,
+		LinearScale,
+		PointElement
+	} from 'chart.js';
+	import { browser } from '$app/environment';
+
+	ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Scatter {data} options={{ responsive: true }} />
